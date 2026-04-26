741d1b99be4c874ec4ceba65da55d07e4994de03
Florent Martinier <florent.martinier@free.fr>
2026-04-26 10:39:24 +0200

    Improve PDF export

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,14 @@
-import { Component } from '@angular/core';
+import { NgForOf, NgIf } from '@angular/common';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { default as html2canvas } from 'html2canvas';
+import { jsPDF } from 'jspdf';
+import { EducationComponent } from "./components/education/education.component";
+import { ExperienceComponent } from "./components/experience/experience.component";
+import { ProjectComponent } from "./components/project/project.component";
 import { CV } from './models/cv.model';
 import { CvService } from './services/cv.services';
-import { ExperienceComponent } from "./components/experience/experience.component";
-import { EducationComponent } from "./components/education/education.component";
-import { ProjectComponent } from "./components/project/project.component";
-import { NgIf, NgForOf } from '@angular/common';
+import { toCanvas } from 'html-to-image';
 
 @Component({
   selector: 'app-root',
@@ -20,6 +23,8 @@ import { NgIf, NgForOf } from '@angular/common';
   templateUrl: './app.component.html'
 })
 export class AppComponent {
+  @ViewChild('cvContent') cvContent!: ElementRef;
+
   title = 'mon_cv';
 
   cv!: CV;
@@ -28,12 +33,40 @@ export class AppComponent {
 
   ngOnInit(): void {
     this.cvService.getCV().subscribe(data => {
-      console.log('mon cv trouvé : ', data);
       this.cv = data;
     });
   }
 
-  downloadPDF() {
-    window.print();
+  public async downloadPDF() {
+    const element = this.cvContent.nativeElement;
+
+    try {
+      const canvas = await toCanvas(element, {
+        quality: 1,
+        pixelRatio: 2, // Pour la netteté
+        backgroundColor: '#ffffff',
+        style: {
+          borderRadius: '0' // On force les coins carrés
+        }
+      });
+
+      const imgData = canvas.toDataURL('image/png');
+
+      // 2. Calcul des dimensions PDF
+      const pdfWidth = 210;
+      const pdfHeight = (canvas.height * pdfWidth) / canvas.width;
+
+      const pdf = new jsPDF({
+        orientation: 'p',
+        unit: 'mm',
+        format: [pdfWidth, pdfHeight]
+      });
+
+      pdf.addImage(imgData, 'PNG', 0, 0, pdfWidth, pdfHeight);
+      pdf.save('CV_Florent_Martinier.pdf');
+
+    } catch (error) {
+      console.error('Erreur lors de la génération du PDF', error);
+    }
   }
 }
